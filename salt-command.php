f336fc298b0e094502ce40e010b23d94aba29fb0
Sam Stoller <snstoller@gmail.com>
2015-02-18 17:27:22 -0800

    Beautify code + standardize copy
    * Beautify code to WP standards; remove trailing spaces
    * Standardize punctuation + casing

--- salt-command.php
+++ salt-command.php
@@ -8,8 +8,8 @@
 class Salts_Command extends WP_CLI_Command {
 
   /**
-   * Generates salts to STDOUT or to a file
-   * 
+   * Generates salts to STDOUT or to a file.
+   *
    * @when before_wp_load
    *
    * @synopsis [--file=<foo>]
@@ -17,19 +17,19 @@ class Salts_Command extends WP_CLI_Command {
    */
   function generate( $args, $assoc_args ) {
     $api  = 'https://api.wordpress.org/secret-key/1.1/salt/';
-    $data = file_get_contents( $api ); 
+    $data = file_get_contents( $api );
 
     if ( isset( $assoc_args['file'] ) ) {
       $file   = $assoc_args['file'];
       $output = '<?php' . PHP_EOL . PHP_EOL . $data . PHP_EOL;
 
       if ( ! is_writable( $file ) )
-        WP_CLI::error('File is not writable or path is not correct: ' . $file );
+        WP_CLI::error( 'File is not writable or path is not correct: ' . $file );
 
       if ( ! file_put_contents( $file, $output ) )
-        WP_CLI::error('could not write salts to: ' . $file );
+        WP_CLI::error( 'Could not write salts to: ' . $file );
 
-      WP_CLI::success('Added salts to: ' . $file );
+      WP_CLI::success( 'Added salts to: ' . $file );
       return;
     }
 
