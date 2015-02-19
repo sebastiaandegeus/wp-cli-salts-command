8b6963ff203942c05932a101f90694dda4b150e2
Sam Stoller <snstoller@gmail.com>
2015-02-18 21:06:49 -0800

    Add new --format flag with 'php' and 'env' options
    * Add private static function _format_data()
    * Update DocBlock + @synopsis tag

--- salt-command.php
+++ salt-command.php
@@ -10,18 +10,30 @@ class Salts_Command extends WP_CLI_Command {
   /**
    * Generates salts to STDOUT or to a file.
    *
-   * @when before_wp_load
+   * ## OPTIONS
    *
-   * @synopsis [--file=<foo>]
+   * [--file=<file>]
+   * : The name of the file to write to. Default outputs to STDOUT.
+   *
+   * [--format=<format>]
+   * : Can be php or env. Defaults to php.
+   *
+   * @when before_wp_load
+   * @synopsis [--file=<file>] [--format=<format>]
    *
    */
   function generate( $args, $assoc_args ) {
-    $api  = 'https://api.wordpress.org/secret-key/1.1/salt/';
-    $data = file_get_contents( $api );
+    $defaults = array(
+      'format' => 'php',
+    );
+    $assoc_args = array_merge( $defaults, $assoc_args );
+
+    $api    = 'https://api.wordpress.org/secret-key/1.1/salt/';
+    $data   = file_get_contents( $api );
+    $output = self::_format_data( $data, $assoc_args['format'] );
 
     if ( isset( $assoc_args['file'] ) ) {
-      $file   = $assoc_args['file'];
-      $output = '<?php' . PHP_EOL . PHP_EOL . $data . PHP_EOL;
+      $file = $assoc_args['file'];
 
       if ( ! is_writable( $file ) )
         WP_CLI::error( 'File is not writable or path is not correct: ' . $file );
@@ -33,7 +45,23 @@ class Salts_Command extends WP_CLI_Command {
       return;
     }
 
-    fwrite( STDOUT, $data );
+    fwrite( STDOUT, $output );
+  }
+
+  private static function _format_data( $data, $format ) {
+    switch ( $format ) {
+      case 'env':
+        $pattern   = "/define\('([A-Z_]+)',\s*'(.+)'\);/";
+        $formatted = "\n\n" . preg_replace($pattern, "$1='$2'", $data) . "\n";
+        break;
+
+      case 'php':
+      default:
+        $formatted = '<?php' . PHP_EOL . PHP_EOL . $data . PHP_EOL;
+        break;
+    }
+
+    return $formatted;
   }
 }
 
