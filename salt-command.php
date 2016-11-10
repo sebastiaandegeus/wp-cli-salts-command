17efb830c104fb3d605be3872fd03a9f093a8cd2
Andrew Wilhelm <andrewwilhelmex@gmail.com>
2016-11-09 19:07:15 -0800

    adding yaml as an output format

--- salt-command.php
+++ salt-command.php
@@ -9,15 +9,32 @@ class Salts_Command extends WP_CLI_Command {
 
   /**
    * Generates salts to STDOUT or to a file
-   * 
+   *
    * @when before_wp_load
    *
-   * @synopsis [--file=<foo>]
+   * @synopsis [--file=<foo>, --format=<php,yaml>]
    *
    */
   function generate( $args, $assoc_args ) {
     $api  = 'https://api.wordpress.org/secret-key/1.1/salt/';
-    $data = file_get_contents( $api ); 
+    $data = file_get_contents( $api );
+
+    if ( isset( $assoc_args['format'] ) ) {
+      $format = $assoc_args['format'];
+      switch ($format) {
+        case 'yaml':
+          $data = str_replace("define('AUTH_KEY',", "auth_key:", $data);
+          $data = str_replace("define('SECURE_AUTH_KEY',","secure_auth_key:", $data);
+          $data = str_replace("define('LOGGED_IN_KEY',","logged_in_key:", $data);
+          $data = str_replace("define('NONCE_KEY',","nonce_key:", $data);
+          $data = str_replace("define('SECURE_AUTH_SALT',","secure_auth_salt:", $data);
+          $data = str_replace("define('LOGGED_IN_SALT',","logged_in_salt:", $data);
+          $data = str_replace("define('NONCE_SALT',","nonce_salt:", $data);
+          $data = str_replace("');","\"", $data);
+          $data = str_replace("'","\"", $data);
+          break;
+      }
+    }
 
     if ( isset( $assoc_args['file'] ) ) {
       $file   = $assoc_args['file'];
